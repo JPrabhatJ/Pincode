bcca4ff91ee75343b978bbf41a160a9ee59d3a65
Prabhat Jha <jhaprabhat268@gmail.com>
2025-06-04 19:02:51 +0530

    New Changes

--- src/Components/Template.jsx
+++ src/Components/Template.jsx
@@ -5,7 +5,7 @@ function Template() {
      <section className='py-12 bg-purple-200'>
         <div className='container px-4 mx-auto text-center'>
             <h2 className='text-3xl font-bold mb-4'>Free Delivery on your first order</h2>
-            <p className='text-lg mb-6 max-w-2xl mx-auto'>Use Code FRESH10 for 
+            <p className='text-lg mb-6 max-w-2xl mx-auto'>Use Code PINCODE for 
                 free delivery on order above ₹199
             </p>
         </div>
